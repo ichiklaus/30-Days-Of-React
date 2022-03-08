8628b2d2e51563dca3f0f4ae3539b1fb1057f657
ichiklaus <nicola1994n@gmail.com>
2022-03-07 21:13:58 -0500

    Add tech stack, modify layout

--- solutions/day-02/ReactExample/reactApp.js
+++ solutions/day-02/ReactExample/reactApp.js
@@ -10,32 +10,42 @@
 //     padding: '2em',
 // };
 
-
+const DATE_OF_START = `27 of february, 2022`;
+let yearBorn = 1994;
+let currentYear = new Date().getFullYear();
+let currentAge = currentYear - yearBorn;
 
 // Root element to be rendered
 const rootElement = document.querySelector(".root");
 const headerJSX = (
     <header key={headerJSX}>
+        <div className="intro-info-wrapper">
+            {/* <small className="intro-info"> 27 of february, 2022</small> */}
+            <small className="intro-info"> {DATE_OF_START} </small>
+            <small className="intro-info copyright">@ichiklaus on all my social media </small>
+        </div>
         <h1 className="intro-title">Hallo! it's <span className="intro-name">@ichiklaus.</span></h1>
         <h2 className="intro-second-title">Welcome to my 30 days of React journey</h2>
-        <div class="intro-info-wrapper">
-            <small className="intro-info copyright">@ichiklaus on all my social media</small>
-            <small> | </small>
-            <small className="intro-info">27 of february, 2022</small>
-        </div>
-        <p className="intro-description">My real name's Nicolás Moreira, I'm a computer systems engineer undergrad @ Universidad Técnica de Manabí in Portoviejo, Ecuador.
+        <p className="intro-description">My name's Nicolás Moreira, {currentAge}. I'm a computer systems engineer undergrad @ Universidad Técnica de Manabí in Portoviejo, Ecuador.
             At the moment I'm growing my career as a software developer in web development.
         </p>
         <p className="intro-description">Here I will document my path to learning react in this 30 days challenge.</p>
     </header>
 );
+const ICHIKLAUS_TECHS = ['HTML', 'CSS', 'Sass/SCSS', 'JS', 'React'];
+let techsFormatted = ICHIKLAUS_TECHS.map(item => <li key={item} >{item}</li>);
 const mainJSX = (
-    <main key={mainJSX}>
-        <h3 className="description">Prerequisites to get started with react.</h3>
-        <p className="description">Currently, I have a good understanding of HTML, CSS, Sass/SCSS, and JS/ECMA6.</p>
-        <div>
-            <label className="fn-input" htmlFor="firstname">First name </label>
-            <input type="text" id="firstname" placeholder="First Name"></input>
+    <main key={mainJSX} className="main">
+        {/* <h3 className="description subtitle">Prerequisites to get started with react.</h3> */}
+        {/* <p className="description">Currently, I have a good understanding of HTML, CSS, Sass/SCSS, and JS/ECMA6.</p> */}
+        <p className="description ">So far I've been learning and working with technologies such as: </p>
+        <ul className="tech-stack">
+            {techsFormatted}
+        </ul>
+
+        <div className="contact-box">
+            <label className="fn-input" htmlFor="email">Contact me </label>
+            <input type="text" id="email" placeholder="mail@example.com"></input>
         </div>
     </main>
 );
@@ -51,11 +61,13 @@ const footerJSX = (
 
 const mainApp = [
     // <div className='container' >
-    <div className="container" 
+    <div
     // style={header-style}
     >
-        {headerJSX}
-        {mainJSX}
+        <div className="container">
+            {headerJSX}
+            {mainJSX}
+        </div>
         {footerJSX}
     </div>
 ]
