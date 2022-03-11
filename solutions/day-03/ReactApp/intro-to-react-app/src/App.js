d162617ac8c0fe3d91d701eb596b57da54466c0b
ichiklaus <nicola1994n@gmail.com>
2022-03-11 01:54:43 -0500

    Create and style functional components

--- solutions/day-03/ReactApp/intro-to-react-app/src/App.js
+++ solutions/day-03/ReactApp/intro-to-react-app/src/App.js
@@ -11,24 +11,22 @@ let getCurrentAge = () => {
 };
 
 // Header
-const headerJSX = (
-    <header>
-        <div className='intro-info-wrapper'>
-            <small className='intro-info'>{DATE_OF_START}</small>
-            <small className='intro-info copyright'>@ichiklaus on all my social media </small>
-        </div>
-        <div className='intro-img-container'>
-            <img className='intro-img' src={nicolasjpg} alt='Nicolas' />
-        </div>
-        <h1 className="intro-title">Hallo! it's <span className="intro-name">@ichiklaus.</span></h1>
-        <h2 className="intro-second-title">Welcome to my 30 days of React journey</h2>
-        <p className="intro-description">My name's Nicolás Moreira, {getCurrentAge()}. I'm a computer systems engineer undergrad @ Universidad Técnica de Manabí in Portoviejo, Ecuador.
-            At the moment I'm growing my career as a software developer in web development.
-        </p>
-        <p className="intro-description">Here I will document my path to learning react in this 30 days challenge.</p>
-
-    </header>
-);
+let HeaderComponent = () => {
+    return (
+        <header>
+            <div className='intro-info-wrapper'>
+                <small className='intro-info'>{DATE_OF_START}</small>
+                <small className='intro-info copyright'>@ichiklaus on all my social media </small>
+            </div>
+            <div className='intro-img-container'>
+                <img className='intro-img' src={nicolasjpg} alt='Nicolas' />
+            </div>
+            <h1 className="intro-title">Hallo! it's <span className="intro-name">@ichiklaus.</span></h1>
+            <h2 className="intro-second-title">Welcome to my 30 days of React journey</h2>
+            <p className="intro-description">I'm a computer systems engineer based in Ecuador passionate and fully interested in web development and UI/UX.</p>
+        </header>
+    );
+};
 
 // Main
 let gettechsFormatted = () => {
@@ -37,46 +35,104 @@ let gettechsFormatted = () => {
         return <li key={item}>{item}</li>
     });
 };
+const myName = {
+    firstname: 'Nicolás',
+    lastname: 'Moreira',
+}
 
-const mainJSX = (
-    <main>
-        <p className="description ">So far I've been learning and working with technologies such as: </p>
-        <ul className="tech-stack">
-            {gettechsFormatted()}
-        </ul>
+let SubscribeButtonComponent = () => {
+    return (
+        <button type='button'>Subscribe</button>
+    );
+}
 
-        <div className="contact-box">
-            <label className="fn-input" htmlFor="email">Contact me </label>
-            <input type="text" id="email" placeholder="mail@example.com"></input>
-        </div>
-    </main>
-);
+let MainComponent = () => {
+    return (
+        <main>
+            {/* Section about me */}
+            <section className="about-me-section">
+                <div className="about-me-header">
+                    <h2>/ about me</h2>
+                </div>
+                <p className="intro-description">My name's {myName.firstname} {myName.lastname}, {getCurrentAge()}. I'm currently working on my theses for my degree in Computer Systems Engineering at Universidad Técnica de Manabí of Portoviejo - Ecuador, while growing my career as a software engineer in web development.
+                </p>
+                <p className="intro-description">At some point I would like to design and develop videogames ever since I caught interest while experimenting with the Unity Game Engine.</p>
+                <p className="intro-description">Here I will document my path to learning react in this 30 days challenge.</p>
+            </section>
+            {/* Section tech stack */}
+            <section className="skills-section">
+                <p className="description ">So far I've been learning and working with technologies such as: </p>
+                <ul className="tech-stack">
+                    {gettechsFormatted()}
+                </ul>
+            </section>
+            <section className="contact-me-section">
+                <div className="contact-box">
+                    {/* <label className="fn-input" htmlFor="email">Contact me </label> */}
+                    <input type="text" id="email" placeholder="mail@example.com"></input>
+                    {SubscribeButtonComponent()}
+                </div>
+                <div>
+                    {HexaColor()}
+                </div>
+            </section>
+        </main>
+    );
+}
+
+let HexaColor = () => {
+    let str = '0123456789abcdef'
+    let color = ''
+    for (let i = 0; i < 6; i++) {
+        let index = Math.floor(Math.random() * str.length)
+        color += str[index]
+    }
+    return (
+        <p className="intro-description">
+            A random hex color picker: {'#' + color}
+        </p>
+    );
+}
 
 // Footer
-const footerJSX = (
-    <footer>
-        <small className="social-links">@ichiklaus on
-            <a href="http://Instagram.com/ichiklaus" target="_blank" rel="noopener noreferrer"> Instagram</a> |
-            <a href="http://github.com/ichiklaus" target="_blank" rel="noopener noreferrer"> Github</a> |
-            <a href="http://frontendmentor.io/profile/ichiklaus" target="_blank" rel="noopener noreferrer"> Frontend Mentor</a>
-        </small>
-    </footer>
-);
+let FooterComponent = () => {
+    return (
+        <footer>
+            <small className="social-links">@ichiklaus on
+                <a href="http://Instagram.com/ichiklaus" target="_blank" rel="noopener noreferrer"> Instagram</a> |
+                <a href="http://github.com/ichiklaus" target="_blank" rel="noopener noreferrer"> Github</a> |
+                <a href="http://frontendmentor.io/profile/ichiklaus" target="_blank" rel="noopener noreferrer"> Frontend Mentor</a>
+            </small>
+        </footer>
+    );
+}
 
-const mainApp = [
-    <div>
+// const mainApp = [
+//     <div>
+//         <div className='container'>
+//             {HeaderComponent()}
+//             {MainComponent()}
+//         </div>
+//         {FooterComponent()}
+//     </div>
+// ];
+
+let main = () => {
+    return (
         <div className='container'>
-            {headerJSX}
-            {mainJSX}
-            
+            <div>
+                <HeaderComponent />
+                <MainComponent />
+            </div>
+            <FooterComponent />
         </div>
-        {footerJSX}
-    </div>
-];
+    );
+}
 
 export default function App() {
     return (
-        mainApp
+        // mainApp
+        main()
     );
 };
 //export default App;
