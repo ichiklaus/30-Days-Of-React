15af94e47d3af566d5c01ffbd1a76aa517906094
ichiklaus <nicola1994n@gmail.com>
2022-03-15 15:59:46 -0500

    Add css imports to index

--- solutions/day-03/ReactApp/intro-to-react-app/src/index.js
+++ solutions/day-03/ReactApp/intro-to-react-app/src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import './styles/index.css'
+import 'normalize.css';
+import './styles/index.css';
+import './styles/App.css';
+import './styles/Mobile.css';
+import './styles/Variables.css';
+
 
 // Root element to be rendered
  const rootElement = document.getElementById('root');
