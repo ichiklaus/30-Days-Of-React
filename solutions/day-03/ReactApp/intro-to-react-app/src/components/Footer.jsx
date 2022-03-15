6a2e2a5c347bd5e850781ffa37673f6b9a7d8d0f
ichiklaus <nicola1994n@gmail.com>
2022-03-15 11:39:33 -0500

    Destructure props for day05

--- solutions/day-03/ReactApp/intro-to-react-app/src/components/Footer.jsx
+++ solutions/day-03/ReactApp/intro-to-react-app/src/components/Footer.jsx
@@ -1,16 +1,44 @@
-import React from 'react';
+import React from "react";
 
 // Footer
-let Footer = () => {
-    return (
-        <footer>
-            <small className="social-links">@ichiklaus on
-                <a href="http://Instagram.com/ichiklaus" target="_blank" rel="noopener noreferrer"> Instagram</a> |
-                <a href="http://github.com/ichiklaus" target="_blank" rel="noopener noreferrer"> Github</a> |
-                <a href="http://frontendmentor.io/profile/ichiklaus" target="_blank" rel="noopener noreferrer"> Frontend Mentor</a>
-            </small>
-        </footer>
-    );
-}
+let Footer = ({
+  myInfo: {
+    author: { username },
+  },
+}) => {
+  return (
+    <footer>
+      <small className="social-links">
+        {username} on
+        <a
+          href={`http://Instagram.com/${username}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {" "}
+          Instagram
+        </a>{" "}
+        |
+        <a
+          href={`http://github.com/${username}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {" "}
+          Github
+        </a>{" "}
+        |
+        <a
+          href={`http://frontendmentor.io/profile/${username}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {" "}
+          Frontend Mentor
+        </a>
+      </small>
+    </footer>
+  );
+};
 
 export default Footer;
