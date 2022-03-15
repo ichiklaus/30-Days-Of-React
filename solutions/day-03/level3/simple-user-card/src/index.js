a21dd548849622d156e80e1350714addede735ca
ichiklaus <nicola1994n@gmail.com>
2022-03-15 15:57:22 -0500

    Add imports

--- solutions/day-03/level3/simple-user-card/src/index.js
+++ solutions/day-03/level3/simple-user-card/src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import 'normalize.css';
+import './styles/index.css';
+import './styles/App.css';
+
 
 ReactDOM.render(
   <React.StrictMode>
