fcefaf59232b26a72cae7e97da48331e43ef10c5
ichiklaus <nicola1994n@gmail.com>
2022-03-15 15:54:57 -0500

    Create Form component

---
+++ solutions/day-03/level2/subscribe-form-single-page/src/components/Form.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Form() {
+    return (
+        <div className='subscribe__inputs'>
+        <input type='text' placeholder='First name'></input>  
+        <input type='text' placeholder='Last name'></input>  
+        <input type='text' placeholder='Email'></input>  
+      </div>
+    );
+}
+
+export default Form;
