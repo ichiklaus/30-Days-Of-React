d824ed0b26b0b6f028194c42864a7ac9111e21b6
ichiklaus <nicola1994n@gmail.com>
2022-03-15 20:28:29 -0500

    Create misc data

---
+++ solutions/day-06/mapping-arrays/src/services/misc/index.js
@@ -0,0 +1,18 @@
+const numbersArr = [1, 2, 3, 4, 5, 6];
+
+const skillStack = [
+    ['HTML', 10],
+    ['CSS', 7],
+    ['JavaScript', 9],
+    ['React', 8],
+];
+
+const countries = [
+    { name: 'Finland', city: 'Helsinki' },
+    { name: 'Sweden', city: 'Stockholm' },
+    { name: 'Denmark', city: 'Copenhagen' },
+    { name: 'Norway', city: 'Oslo' },
+    { name: 'Iceland', city: 'Reykjavík' },
+  ]
+
+export { numbersArr, skillStack, countries };
