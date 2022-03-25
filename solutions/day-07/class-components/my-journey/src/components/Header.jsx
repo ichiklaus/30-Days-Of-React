24e68fcddcf8c3b81e57bbc48ba1d952ce867880
Nicolas Moreira <nicola1994@hotmail.es>
2022-03-25 11:19:02 -0500

    Move Controls to Header

--- solutions/day-07/class-components/my-journey/src/components/Header.jsx
+++ solutions/day-07/class-components/my-journey/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import nicolasjpg from "../images/me.jpg";
+import { myInfo } from "../services/common/common";
 
 const STARTING_DATE = "27 of february of 2022";
 
@@ -9,21 +10,94 @@ class Header extends React.Component {
     // Creating state for Component Props
     this.state = {
       welcometext: props.welcomeSection.welcome,
-      // prefersDarkTheme: window.matchMedia("(prefers-color-scheme: dark)")
-      //   .matches,
-      // icon: document.getElementById("theme-toggler"),
-      // themeClassname: this.themeState ? "fas fa-moon" : "fas fa-sun",
       themeState: false,
+      techStack: myInfo.techStack,
+      loggedIn: false,
+      message: "",
     };
   }
 
-  // toggleLightMode = () => {
-  //   if (this.state.prefersDarkTheme) {
-  //     document.body.classList.add("light-theme");
-  //   } else {
-  //     document.body.classList.remove("light-theme");
-  //   }
-  // };
+  // An Action Button component
+  ActionButton = ({ text, actions }) => {
+    return (
+      <button
+        className={
+          text === `Subscribe`
+            ? `button button--submit`
+            : `button button--submit margin`
+        }
+        onClick={actions}
+      >
+        {text}
+      </button>
+    );
+  };
+
+  // Used to handle login state
+  handleLogin = () => {
+    this.setState({
+      loggedIn: !this.state.loggedIn,
+    });
+    console.log(
+      "🚀 ~ file: App.js ~ line 22 ~ App ~ this.state.loggedIn",
+      this.state.loggedIn
+    );
+  };
+
+  // Displays current state for login handler
+  Status = () => {
+    return this.state.loggedIn ? (
+      <h3 className="welcome-message">
+        Welcome to my 30 days of React journey
+      </h3>
+    ) : (
+      <h3 className="welcome-message">Please, log in to continue</h3>
+    );
+  };
+
+  GettingStarted = () => {
+    return (
+      <h3 id="login-message">
+        Please login to access more information about 30 Days Of React challenge
+      </h3>
+    );
+  };
+
+  Prerrequistes = () => {
+    return <h3 id="login-message">You have all the prerequisite courses to get started with React</h3>;
+  };
+
+  // Display messages
+  Message = () => <h5> {this.state.message} </h5>;
+
+  // For datetime display function
+  showDate = (time) => {
+    const months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    const month = months[time.getMonth()];
+    const year = time.getFullYear();
+    const date = time.getDate();
+    return `${date} of ${month} of ${year}`;
+  };
+
+  // State for datetime handler
+  datetimeHandler = () => {
+    this.state.message === ""
+      ? this.setState({ message: this.showDate(new Date()) })
+      : this.setState({ message: "" });
+  };
 
   toggleLightMode = () => {
     if (!this.state.themeState) {
@@ -40,12 +114,34 @@ class Header extends React.Component {
   };
 
   render() {
+    let loggedInText = this.state.loggedIn ? "Logout" : "Log in";
     const {
       author: { username },
       countryBasedin,
     } = this.props.myInfo;
     return (
-      <div className="header-wrapper">
+      <div id="home" className="header-wrapper">
+        <div className="login-wrapper">
+          <div className="action-wrapper">
+            <this.ActionButton text={loggedInText} actions={this.handleLogin} />
+            <this.ActionButton
+              text={`Show time`}
+              actions={this.datetimeHandler}
+            />
+          </div>
+          {/* Conditional Rendering using &&  */}
+          {!this.state.loggedIn && ( // Checks the login status, if it's not logged in, it displays Please login text from GettingStarted Component
+            <this.GettingStarted />
+          )}
+          {/* <this.Status /> */}
+          {this.state.techStack.length >= 3 && this.state.loggedIn && (
+            <this.Prerrequistes />
+          )}
+          <div className="display-datetime">
+            <this.Message message={this.state.message} />
+          </div>
+        </div>
+
         <header>
           <div className="intro-info-container">
             <div className="intro-info-top">
@@ -57,14 +153,14 @@ class Header extends React.Component {
                 ></i>
                 <small className="intro-info"> {STARTING_DATE} </small>
                 <small className="intro-info">
-                  <span className="copyright">@{username}</span> on all my social
-                  media
+                  <span className="copyright">@{username}</span> on all my
+                  social media
                 </small>
               </div>
             </div>
             <nav id="side-navbar">
               <div className="nav-wrapper">
-                <a href="#">/home</a>
+                <a href="#home">/home</a>
                 <a href="#about-me-section">/about</a>
                 <a href="#topics-section">/journal</a>
               </div>
